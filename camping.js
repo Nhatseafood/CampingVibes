3bd346bd28baee449790ec6b728573500ae5a099
Cristobal <manzanares.cristobal@gmail.com>
2018-03-16 15:33:01 -0500

    Geolocation, and reverse geo API's functional

--- camping.js
+++ camping.js
@@ -4,6 +4,7 @@ var apiKey = 'bc9t36mudawubc2ajzkzne38';
 var apiKey2 = 'HVQ3QUEPTRX3FKFXDMBAEHSZ/* '
 var googleCustomSearchApi = 'AIzaSyBDvkJWRxyi3U8yPxLoJClo2Zm9mPyjRUo'
 var googleCustomEngine = '016068319718830097589:iw357dqkr-a'
+var googleGeolocationAPi = 'AIzaSyCFaYviqNM0Ts59qcR5IV652b3C29xaF9g'
 
 
 // var url = 'http://api.amp.active.com/camping/campgrounds/?pstate=tx&api_key=bc9t36mudawubc2ajzkzne38';
@@ -11,8 +12,8 @@ var googleCustomEngine = '016068319718830097589:iw357dqkr-a'
 
 
 
-//API call using jquery
-/*
+//API call using jquery. Calls americareserve website by using the active access API
+function Main(){
 $.ajax({
 
     url: 'http://api.amp.active.com/camping/campgrounds/?pstate=TX&api_key=bc9t36mudawubc2ajzkzne38',
@@ -21,7 +22,7 @@ $.ajax({
     dataType: 'xml',
     success: function(respond) {
         var x = respond
-        /*var y = x.getElementsByTagName("resultset")[0]
+        var y = x.getElementsByTagName("resultset")[0]
         var z = y.getElementsByTagName('result')[0].getAttribute('faciltyPhoto')
         
         //console.log(x)
@@ -32,24 +33,11 @@ $.ajax({
     },
     error: function() { alert('Failed!'); },
 });
+}
 
-*/
-function hndlr(response) {
-    for (var i = 0; i < response.items.length; i++) {
-      var item = response.items[i];
-      // in production code, item.htmlTitle should have the HTML entities escaped.
-      document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
-    }
-  }
 
-/*
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://www.googleapis.com/customsearch/v1?q=cars&cx=016068319718830097589%3Aiw357dqkr-a&imgColorType=color&num=3&searchType=image&key=AIzaSyBDvkJWRxyi3U8yPxLoJClo2Zm9mPyjRUo", false);
-  xhr.send();
-  console.log(xhr.status);
-console.log(xhr.statusText);
-*/
 
+// Image getter for the campsites using a custom google search engine
 function campImg(campName) {
     var t= campName;
 $.ajax({
@@ -59,23 +47,74 @@ $.ajax({
     dataType: 'json',
     success: function(respond) {
         var x = respond
-        /*var y = x.getElementsByTagName("resultset")[0]
-        var z = y.getElementsByTagName('result')[0].getAttribute('faciltyPhoto')
-        */
          console.log(x['items'][0]['link']);
          var z = x['items'][0]['link']; 
          var a = document.getElementById('info')
          var b = document.createElement("IMG");
          b.setAttribute("src",z);
-         a.appendChild(b);
-
-       
-    
-        
-        
-        
+         a.appendChild(b);   
     },
     error: function(){ alert('Failed!'); },
 });
 }
-campImg('car')
+
+
+function initMap() {
+        console.log('Hello')
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+        
+        });
+        var geocoder = new google.maps.Geocoder;
+        var infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            console.log(pos)
+            geocoder.geocode({'location': pos}, function(results, status) {
+                if (status === 'OK') {
+                  if (results[0]) {
+                    console.log(results[9])
+                    
+                    
+                  } else {
+                    window.alert('No results found');
+                  }
+                } else {
+                  window.alert('Geocoder failed due to: ' + status);
+                }})
+
+
+
+
+
+
+
+
+
+
+
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);}
+
+
